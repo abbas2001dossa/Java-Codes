47088c4f85a8d31649800e7cbab238342afed3fa
abbas2001dossa <84897268+abbas2001dossa@users.noreply.github.com>
2022-10-05 09:59:35 -0700

    Add files via upload

---
+++ five.java
@@ -0,0 +1,12 @@
+public class five {
+    public static void main(String[] args) {
+
+    
+        for ( int i=0 ; i > 0 ; i++)
+        {
+            System.out.println("Array elements are :- ");
+        }
+
+
+    }
+}
